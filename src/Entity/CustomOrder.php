f3c8db21cec5e2565c2f540fcbf886a59c7c9ffa
Antonin <antonin63170@gmail.com>
2025-05-16 09:09:08 +0200

    Update Relation CustomOrder -> User

--- src/Entity/CustomOrder.php
+++ src/Entity/CustomOrder.php
@@ -35,6 +35,10 @@ class CustomOrder
     #[ORM\ManyToOne(inversedBy: 'customOrder')]
     private ?WoodType $woodType = null;
 
+    #[ORM\ManyToOne(inversedBy: 'customOrder')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -120,6 +124,18 @@ class CustomOrder
     public function setWoodType(?WoodType $woodType): static
     {
         $this->woodType = $woodType;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
 
         return $this;
     }
