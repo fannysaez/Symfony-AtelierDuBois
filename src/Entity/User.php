6a0f8a785587dde9d98c00aebbf182df5319706d
Antonin <antonin63170@gmail.com>
2025-05-16 09:08:15 +0200

    Update Relation CustomOrder -> User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -38,6 +40,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255)]
     private ?string $lastName = null;
+
+    /**
+     * @var Collection<int, CustomOrder>
+     */
+    #[ORM\OneToMany(targetEntity: CustomOrder::class, mappedBy: 'user')]
+    private Collection $customOrder;
+
+    public function __construct()
+    {
+        $this->customOrder = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -132,6 +145,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setLastName(string $lastName): static
     {
         $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CustomOrder>
+     */
+    public function getCustomOrder(): Collection
+    {
+        return $this->customOrder;
+    }
+
+    public function addCustomOrder(CustomOrder $customOrder): static
+    {
+        if (!$this->customOrder->contains($customOrder)) {
+            $this->customOrder->add($customOrder);
+            $customOrder->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCustomOrder(CustomOrder $customOrder): static
+    {
+        if ($this->customOrder->removeElement($customOrder)) {
+            // set the owning side to null (unless already changed)
+            if ($customOrder->getUser() === $this) {
+                $customOrder->setUser(null);
+            }
+        }
 
         return $this;
     }
