96894e2147a276d159c58d6e58b9d236115dd0e6
Antonin <antonin63170@gmail.com>
2025-05-16 03:37:47 +0200

    Update CustomOrder

--- src/Controller/CustomOrderController.php
+++ src/Controller/CustomOrderController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\CustomOrder;
-use App\Form\CustomOrderTypeForm;
 use DateTime;
 use DateTimeImmutable;
+use App\Entity\CustomOrder;
+use App\Form\CustomOrderTypeForm;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-
 
 final class CustomOrderController extends AbstractController
 {
@@ -23,7 +22,7 @@ final class CustomOrderController extends AbstractController
     // }
 
     #[Route('/custom-order-plank', name: 'custom_order_plank')]
-    public function addCustomerOrder(Request $request, EntityManagerInterface $em): Response
+    public function addCustomerOrder(Request $request, EntityManagerInterface $em, ProductRepository $productRepository): Response
     {
         $customOrder = new CustomOrder();
 
@@ -38,10 +37,15 @@ final class CustomOrderController extends AbstractController
             $em->flush();
         }
 
+        $latestProducts = $productRepository->findBy(
+            [],
+            ['createdAt' => 'DESC'],
+            3
+        );
 
         return $this->render('custom_order/custom_order.html.twig', [
-
             'form' => $form->createView(),
+            'latestProducts' => $latestProducts,
         ]);
     }
 }
