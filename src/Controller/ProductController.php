dc17d04ce97c880f30ee9ea127e920e5d9baba33
Antonin <antonin63170@gmail.com>
2025-05-16 14:46:50 +0200

    Add Category & Product Page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Entity\Category;
 use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,20 +14,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class ProductController extends AbstractController
 {
     #[Route('', name: 'product_index')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
         $products = $productRepository->findBy([], ['createdAt' => 'DESC']);
+        $categories = $categoryRepository->findAll();
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/{id}', name: 'product_category')]
+    public function category(
+        ProductRepository $productRepository,
+        CategoryRepository $categoryRepository,
+        Category $category
+    ): Response {
+
+        $products = $productRepository->findBy(
+            ['category' => $category],
+        );
+
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+            'categories' => $categories,
+            'current_category' => $category
         ]);
     }
 
     #[Route('/{id}', name: 'product_show')]
-    public function show(Product $product): Response
+    public function show(Product $product, ProductRepository $productRepository): Response
     {
+        $latestProducts = $productRepository->findBy(
+            [],
+            ['createdAt' => 'DESC'],
+            3
+        );
+
         return $this->render('product/show.html.twig', [
             'product' => $product,
+            'latestProducts' => $latestProducts,
         ]);
     }
 }
